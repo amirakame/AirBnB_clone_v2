690529e9ed48a8d668b157efe3f4645409dc8751
amirakame <amirakamel343@gmail.com>
2023-10-22 17:26:53 -0700

    chool

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-"""Starts  Flask web application.
-
-application listens on 0.0.0.0, port 5000.
-Routes:
-/: Displays 'Hello HBNB!'
-"""
+""" Starts a Flash Web Application """
 from flask import Flask
-
 app = Flask(__name__)
 
 
-	@app.route("/", strict_slashes=False)
-	def hello_hbnb():
-		"""Displays 'Hello HBNB!'"""
-			return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
 
-
-			if __name__ == "__main__":
-			app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
