af5fdb37762b98f6d0c529b6628c1243089c5b0f
amirakame <amirakamel343@gmail.com>
2023-10-22 17:33:54 -0700

    chool

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-"""Starts  Flask web application.
-
- application listens on 0.0.0.0, port 5000.
- Routes:
-     /: Displays 'Hello HBNB!'.
-         /hbnb: Displays 'HBNB'.
-	 """
-	 from flask import Flask
-
-	 app = Flask(__name__)
+""" Starts a Flash Web Application HBNB"""
+from flask import Flask
+app = Flask(__name__)
 
 
-	@app.route("/", strict_slashes=False)
-	def hello_hbnb():
-		    """Displays 'Hello HBNB!'."""
-			        return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
 
 
-				@app.route("/hbnb", strict_slashes=False)
-				def hbnb():
-					    """Displays 'HBNB'."""
-						        return "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Prints a Message when /hbnb is called """
+    return 'HBNB'
 
-
-							if __name__ == "__main__":
-							    app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
